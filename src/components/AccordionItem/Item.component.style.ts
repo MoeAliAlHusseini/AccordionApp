9cfd914234ee7ee0712930b8b9515ad169b220bd
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2023-01-31 22:15:19 +0400

    Refactor the item styling component

---
+++ src/components/AccordionItem/Item.component.style.ts
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderRadius: 4,
+    borderWidth: 1,
+  },
+  touchableOpacityContainer: {
+    width: '100%',
+  },
+  title: {
+    marginBottom: 20,
+  },
+  description: {
+    marginBottom: 10,
+  },
+});
+
+export default styles;
