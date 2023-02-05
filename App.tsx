935c40b4493b0bbf8fe5b40fbc5de4b72529b041
MoeAliAlHusseini <mohamadalialhusseini94@gmail.com>
2023-02-05 17:26:47 +0400

    Import provider and store to app index

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 
+import store from './src/store';
+import { Provider } from 'react-redux';
+
 import Router from './src/router';
 
 function App() {
-    return <Router />;
+    return (
+        <Provider store={store}>
+            <Router />
+        </Provider>
+    );
 }
 
 export default App;
